6611a4bcd14c115a3adeb2588d9526d9b38d3ce0
Panicia <kkk2224@yandex.ru>
2022-05-09 14:25:06 +0300

    good job1

--- parceVac.py
+++ parceVac.py
@@ -14,35 +14,121 @@ class parseVac():
         self.daily_vaccinations_raw = []
         self.daily_vaccinations = []
         self.total_vaccinations_per_hundred = []
-        self.people_vaccinations_per_hundred = []
+        self.people_vaccinated_per_hundred = []
         self.people_fully_vaccinated_per_hundred = []
         self.total_boosters_per_hundred = []
         self.daily_vaccinations_per_million = []
         self.daily_people_vaccinated = []
         self.daily_people_vaccinated_per_hundred = []
 
-        self.data = [
-                     self.location, self.iso_code, self.date,
-                     self.total_vaccinations, self.people_vaccinated,
-                     self.people_fully_vaccinated, self.total_boosters,
-                     self.daily_vaccinations_raw, self.daily_vaccinations,
-                     self.total_vaccinations_per_hundred, self.people_vaccinations_per_hundred,
-                     self.people_fully_vaccinated_per_hundred, self.total_boosters_per_hundred,
-                     self.daily_vaccinations_per_million, self.daily_people_vaccinated,
-                     self.daily_people_vaccinated_per_hundred
-                    ]
+        self.data = {
+                     'location': self.location, 'iso_code': self.iso_code, 'date': self.date,
+                     'total_vaccinations': self.total_vaccinations, 'people_vaccinated': self.people_vaccinated,
+                     'people_fully_vaccinated': self.people_fully_vaccinated, 'total_boosters': self.total_boosters,
+                     'daily_vaccinations_raw': self.daily_vaccinations_raw, 'daily_vaccinations': self.daily_vaccinations,
+                     'total_vaccinations_per_hundred': self.total_vaccinations_per_hundred, 'people_vaccinated_per_hundred': self.people_vaccinated_per_hundred,
+                     'people_fully_vaccinated_per_hundred': self.people_fully_vaccinated_per_hundred, 'total_boosters_per_hundred': self.total_boosters_per_hundred,
+                     'daily_vaccinations_per_million': self.daily_vaccinations_per_million, 'daily_people_vaccinated': self.daily_people_vaccinated,
+                     'daily_people_vaccinated_per_hundred': self.daily_people_vaccinated_per_hundred
+                    }
 
         with open(self.path, 'r', encoding = 'utf-8') as f:
             i = 0
             for line in f:
                 if i != 0:
                     pieces = re.split(r',|\n', line)
-                    for j in range(len(self.data)):
-                        self.data[j].append(pieces[j])
+                    k = 0
+                    for j in self.data:
+                        if j != 'location' and j != 'iso_code' and j != 'date':
+                            if pieces[k] == '':
+                                self.data[j].append(None)
+                            else:
+                                self.data[j].append(float(pieces[k]))
+                        else:
+                            self.data[j].append(pieces[k])
+                        k += 1
                 i += 1
+    def getDataListPerCountry(self, location, name):
+        datalist = []
+        for i in range(0, len(self.data[name]) - 1):
+            if self.data['location'][i] == location:
+                datalist.append(self.data[name][i])
+        return datalist
 
-
-#parser = parseVac('vaccinations.txt')
-#parser.parseAll()
-#print(parser.location)
+    def getDatalistFromAll(self, name, point = 'start', find_data_if_none = True):
+        datalist = []
+        for i in range(0, len(self.data[name]) - 1):
+            if point == 'start':
+                if i != 0:
+                    if self.data['location'][i - 1] != self.data['location'][i]:
+                        if find_data_if_none:
+                            if not self.data[name][i] is None:
+                                datalist.append(self.data[name][i])
+                            else:
+                                k = 1
+                                while self.data['location'][i + k] == self.data['location'][i]:
+                                    if not self.data[name][i + k] is None:
+                                        datalist.append(self.data[name][i + k])
+                                        break
+                                    if i + k == len(self.data[name]) - 1:
+                                        datalist.append(None)
+                                        break
+                                    k += 1
+                        else:
+                            datalist.append(self.data[name][i])
+                else:
+                    if find_data_if_none:
+                        if not self.data[name][i] is None:
+                                datalist.append(self.data[name][i])
+                        else:
+                            k = 1
+                            while self.data['location'][i + k] == self.data['location'][i]:
+                                if not self.data[name][i + k] is None:
+                                    datalist.append(self.data[name][i + k])
+                                    break
+                                if i + k == len(self.data[name]) - 1:
+                                    datalist.append(None)
+                                    break
+                                k += 1
+                    else:
+                        datalist.append(self.data[name][i])
+            elif point == 'end':
+                if i != len(self.data[name]) - 1:
+                    if self.data['location'][i + 1] != self.data['location'][i]:
+                        if find_data_if_none:
+                            if not self.data[name][i] is None:
+                                datalist.append(self.data[name][i])
+                            else:
+                                k = 1
+                                while self.data['location'][i - k] == self.data['location'][i]:
+                                    if not self.data[name][i - k] is None:
+                                        datalist.append(self.data[name][i - k])
+                                        break
+                                    if i + k == len(self.data[name]) - 1:
+                                        datalist.append(None)
+                                        break
+                                    k += 1
+                        else:
+                            datalist.append(self.data[name][i])
+                else:
+                    if find_data_if_none:
+                        if not self.data[name][i] is None:
+                                datalist.append(self.data[name][i])
+                        else:
+                            k = 1
+                            while self.data['location'][i - k] == self.data['location'][i]:
+                                if not self.data[name][i - k] is None:
+                                    datalist.append(self.data[name][i - k])
+                                    break
+                                if i + k == len(self.data[name]) - 1:
+                                    datalist.append(None)
+                                    break
+                                k += 1
+                    else:
+                        datalist.append(self.data[name][i])
+        return datalist
+'''
+parser = parseVac('vaccinations.txt')
+parser.parseAll()
+print(parser.getDatalist('total_vaccinations', 'end'))'''
     
--- try1.py
+++ try1.py
@@ -1,4 +1,6 @@
 import plotly.graph_objects as go
+import plotly.express as px
+
 from parceVac import parseVac as pV
 
 pv = pV('vaccinations.txt')
@@ -6,8 +8,40 @@ pv.parseAll()
 
 
 
-fig = go.Figure(
-    data=[go.Bar(y=[2, 1, 3], x=[4,5,6])],
-    layout_title_text="A Figure Displayed with fig.show()"
+'''fig = go.Figure(
+    data=[go.Bar(y = pv.getDatalist('daily_people_vaccinated_per_hundred', 'end'), x = pv.getDatalist('location', 'start')), 
+          go.Bar(y = pv.getDatalist('daily_people_vaccinated_per_hundred', 'start'), x = pv.getDatalist('location', 'start'))],
+    layout_title_text = 'daily_people_vaccinated_per_hundred'
 )
+fig.show()'''
+
+'''df = px.data.tips()
+fig = px.histogram(y = pv.getDatalist('daily_people_vaccinated_per_hundred', 'end'), x = pv.getDatalist('location', 'end'))
+fig.show()'''
+
+name = 'people_vaccinated_per_hundred'
+
+X1 = pv.getDataListPerCountry('Latvia', 'date')
+Y1 = pv.getDataListPerCountry('Latvia', name)
+l1 = {'date': X1, name: Y1}
+
+X2 = pv.getDataListPerCountry('Russia', 'date')
+Y2 = pv.getDataListPerCountry('Russia', name)
+l2 = {'date': X2, name: Y2}
+
+X3 = pv.getDataListPerCountry('United States', 'date')
+Y3 = pv.getDataListPerCountry('United States', name)
+l3 = {'date': X3, name: Y3}
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x = X1, y = Y1,
+                    mode='lines',
+                    name='Latvia'))
+fig.add_trace(go.Scatter(x = X2, y = Y2,
+                    mode='lines',
+                    name='Russia'))
+fig.add_trace(go.Scatter(x = X3, y = Y3,
+                    mode='lines',
+                    name='United States'))
+
 fig.show()
